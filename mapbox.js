c6bf79bb97344ae0286dfd16d222dccb3a1045a7
Tom MacWright <tom@macwright.org>
2012-07-03 15:16:31 -0400

    Add simplestyle factory, sketch of mapbox js

---
+++ mapbox.js
@@ -0,0 +1,24 @@
+// mapbox.js
+var mapbox = {};
+
+mapbox.load = function(url, callback) {
+    wax.tilejson(url, function(tj) {
+        var options = {};
+        options.center = { lat: tj.center[1], lon: tj.center[0] };
+        options.zoom = tj.center[2];
+        options.layer = new wax.mm.connector(tj);
+        if (tj.data) {
+            options.markers = mmg().factory(simplestyle_factory).url(tj.data, function() {
+                callback(options);
+            });
+        } else {
+            callback(options);
+        }
+    });
+};
+
+mapbox.markers = mmg;
+
+mapbox.map = function(el, layer) {
+    return new MM.Map(el, layer);
+};
