9aa5a1b0b8210abb5ee92466258ec8b45975d3d1
Tom MacWright <tom@macwright.org>
2012-07-05 18:14:57 -0400

    Bake new min version, start callback binding.

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 build:
-	cat lib/*.js mapbox.js | uglifyjs > mapbox.min.js
+	cat lib/modestmaps.min.js lib/easey.js lib/easey.handlers.js lib/mmg.js lib/mmg_interaction.js lib/simplestyle_factory.js lib/wax.mm.min.js mapbox.js | uglifyjs > mapbox.min.js
--- mapbox.js
+++ mapbox.js
@@ -1,87 +1,99 @@
-// mapbox.js
-var mapbox = {};
+;(function() {
+    // mapbox.js
+    var mapbox = {};
 
-// mapbox.load pulls a [TileJSON](http://mapbox.com/wax/tilejson.html)
-// object from a server and uses it to configure a map and various map-related
-// objects
-mapbox.load = function(url, callback) {
-    wax.tilejson(url, function(tj) {
-        // Pull zoom level out of center
-        tj.zoom = tj.center[2];
+    // mapbox.load pulls a [TileJSON](http://mapbox.com/wax/tilejson.html)
+    // object from a server and uses it to configure a map and various map-related
+    // objects
+    mapbox.load = function(url, callback) {
+        wax.tilejson(url, function(tj) {
+            // Pull zoom level out of center
+            tj.zoom = tj.center[2];
 
-        // Instantiate center as a Modest Maps-compatible object
-        tj.center = { lat: tj.center[1], lon: tj.center[0] };
+            // Instantiate center as a Modest Maps-compatible object
+            tj.center = { lat: tj.center[1], lon: tj.center[0] };
 
-        // Instantiate tile layer
-        if (tj.tiles) tj.layer = new wax.mm.connector(tj);
+            // Instantiate tile layer
+            if (tj.tiles) tj.layer = new wax.mm.connector(tj);
 
-        // Instantiate markers layer
-        if (tj.data) {
-            tj.markers = mmg().factory(simplestyle_factory);
-            tj.markers.url(tj.data, function() {
-                mmg_interaction(tj.markers);
+            // Instantiate markers layer
+            if (tj.data) {
+                tj.markers = mmg().factory(simplestyle_factory);
+                tj.markers.url(tj.data, function() {
+                    mmg_interaction(tj.markers);
+                    callback(tj);
+                });
+            } else {
                 callback(tj);
-            });
-        } else {
-            callback(tj);
-        }
-    });
-};
-
-mapbox.auto = function(el, callback) {
-    return function(options) {
-        var map = mapbox.map(el);
-        if (options.layer) map.addLayer(options.layer);
-        if (options.markers) map.addLayer(options.markers);
-        if (options.attribution) wax.mm.attribution(options.attribution);
-        if (options.legend) wax.mm.legend(options.legend);
-        wax.mm.zoomer(map).appendTo(map.parent);
-        wax.mm.zoombox(map);
-        map.zoom(options.zoom)
-            .center(options.center);
-        wax.mm.interaction()
-            .map(map)
-            .tilejson(options)
-            .on(wax.tooltip().parent(map.parent).events());
-        if (callback) callback(map);
+            }
+        });
     };
-};
 
-mapbox.markers = mmg;
+    // Full auto mode. This can be supplied as the argument to mapbox.load
+    // in order to construct a map from a tilejson snippet.
+    mapbox.auto = function(el, callback) {
+        return function(options) {
+            var map = mapbox.map(el);
+            if (options.layer) map.addLayer(options.layer);
+            if (options.markers) map.addLayer(options.markers);
+            if (options.attribution) wax.mm.attribution(map, options).appendTo(map.parent);
+            if (options.legend) wax.mm.legend(map, options).appendTo(map.parent);
+            wax.mm.zoomer(map).appendTo(map.parent);
+            wax.mm.zoombox(map);
+            map.zoom(options.zoom)
+                .center(options.center);
+            wax.mm.interaction()
+                .map(map)
+                .tilejson(options)
+                .on(wax.tooltip().parent(map.parent).events());
+            if (callback) callback(map);
+        };
+    };
 
-var smooth_handlers = [easey.TouchHandler,
-    easey.DragHandler,
-    easey.DoubleClickHandler,
-    easey.MouseWheelHandler];
+    mapbox.markers = mmg;
 
-var default_handlers = [MM.TouchHandler,
-    MM.DragHandler,
-    MM.DoubleClickHandler,
-    MM.MouseWheelHandler];
+    var smooth_handlers = [easey.TouchHandler,
+        easey.DragHandler,
+        easey.DoubleClickHandler,
+        easey.MouseWheelHandler];
 
-MM.Map.prototype.smooth = function(_) {
-    while (this.eventHandlers.length) {
-        this.eventHandlers.pop().remove();
-    }
-    if (_) {
-        for (var j = 0; j < smooth_handlers.length; j++) {
-            var h = smooth_handlers[j]();
-            this.eventHandlers.push(h);
-            h.init(this);
+    var default_handlers = [MM.TouchHandler,
+        MM.DragHandler,
+        MM.DoubleClickHandler,
+        MM.MouseWheelHandler];
+
+    MM.Map.prototype.smooth = function(_) {
+        while (this.eventHandlers.length) {
+            this.eventHandlers.pop().remove();
         }
-    } else {
-        for (var k = 0; k < default_handlers.length; k++) {
-            var h = default_handlers[k]();
-            this.eventHandlers.push(h);
-            h.init(this);
+        if (_) {
+            for (var j = 0; j < smooth_handlers.length; j++) {
+                var h = smooth_handlers[j]();
+                this.eventHandlers.push(h);
+                h.init(this);
+            }
+        } else {
+            for (var k = 0; k < default_handlers.length; k++) {
+                var h = default_handlers[k]();
+                this.eventHandlers.push(h);
+                h.init(this);
+            }
         }
-    }
-    console.log(this.eventHandlers);
-};
+        return this;
+    };
 
-mapbox.map = function(el, layer) {
-    return new MM.Map(el, layer, null, [easey.TouchHandler(),
-        easey.DragHandler(),
-        easey.DoubleClickHandler(),
-        easey.MouseWheelHandler()]);
-};
+    MM.Map.prototype.on = function(evt, callback) {
+        if (evt in this.callbackManager.callbacks) {
+            this.addCallback(evt, callback);
+        }
+    };
+
+    mapbox.map = function(el, layer) {
+        return new MM.Map(el, layer, null, [easey.TouchHandler(),
+            easey.DragHandler(),
+            easey.DoubleClickHandler(),
+            easey.MouseWheelHandler()]);
+    };
+
+    this.mapbox = mapbox;
+})(this);
