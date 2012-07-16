04b202978c745e1bc1aa455548dffa9abfa99d74
Ansis Brammanis <ansis.brammanis@gmail.com>
2012-07-16 12:04:29 -0400

    Add asyncMap util

---
+++ src/util.js
@@ -0,0 +1,23 @@
+if (typeof mapbox === 'undefined') mapbox = {};
+
+
+mapbox.util = {
+
+    // Asynchronous map that groups results maintaining order
+    asyncMap: function(values, func, callback) {
+        var remaining = values.length,
+            results = [];
+
+        function next(index) {
+            return function(result) {
+                results[index] = result;
+                remaining--;
+                if (!remaining) callback(results);
+            }
+        }
+
+        for (var i = 0; i < values.length; i++) {
+            func(values[i], next(i));
+        }
+    }
+}
