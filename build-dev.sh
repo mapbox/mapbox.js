38fa4f9923e353dc9f65f54bb2a2a69cea94bf9a
Tom MacWright <tom@macwright.org>
2012-07-16 13:51:42 -0400

    Build was accidentally nuked

---
+++ build-dev.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+TAG="dev"
+
+if [ ! -e dist/$TAG ]; then
+    mkdir "dist/$TAG"
+fi
+
+echo "--- BUILDING DEV mapbox.js $TAG ---"
+
+echo "Installing dependencies..."
+echo ""
+echo ""
+npm install
+
+echo "Concatenating mapbox.js..."
+cat node_modules/bean/bean.js \
+	node_modules/mustache/mustache.js \
+	node_modules/reqwest/reqwest.js \
+	node_modules/modestmaps/modestmaps.js \
+	node_modules/wax/lib/html-sanitizer-bundle.js \
+	node_modules/wax/lib/html-sanitizer-loosen.js \
+	node_modules/wax/control/lib/*.js \
+	node_modules/wax/control/mm/*.js \
+	node_modules/wax/connectors/mm/*.js \
+	node_modules/easey/src/easey.js \
+	node_modules/easey/src/easey.handlers.js \
+	node_modules/markers/dist/markers.js \
+	src/mapbox.js src/layer.js > mapbox.js
+
+echo "Minifying mapbox.min.js"
+./node_modules/.bin/uglifyjs mapbox.js > mapbox.min.js
+
+# css
+echo "Concatenating mapbox.css..."
+cat node_modules/markers/dist/markers.css \
+    node_modules/wax/theme/controls.css \
+	theme/mapbox.css > mapbox.min.css
+
+
+# bake a release
+cp mapbox.min.js "dist/$TAG/mapbox.js"
+cp mapbox.js "dist/$TAG/mapbox.uncompressed.js"
+cp mapbox.min.css "dist/$TAG/mapbox.css"
+cp node_modules/wax/theme/map-controls.png "dist/$TAG/map-controls.png"
+cp node_modules/wax/theme/blank.gif "dist/$TAG/blank.gif"
+
+set -- `wc -c mapbox.min.js`
+echo "mapbox.min.js size:"
+units "$1 bytes" "kilobytes"
