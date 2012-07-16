3f58e3f1677efb1db3791bacb5eaee460ea9c8f5
tristen <tristen@tristen.ca>
2012-07-16 08:43:59 -0400

    First theme drop

--- build-site.sh
+++ build-site.sh
@@ -17,17 +17,17 @@ echo "Checking out tag..."
 git checkout $TAG package.json
 
 echo "Installing dependencies..."
-npm install
+#npm install
 
 echo "Creating _posts/$TAG"
 
 mkdir "_posts/$TAG"
 
-cat site/mapbox.yaml API.md \
+cat site/frontmatter/mapbox.yaml API.md \
     > _posts/$TAG/0200-01-01-mapbox.md
 
-cat site/easey.yaml node_modules/easey/API.md \
+cat site/frontmatter/easey.yaml node_modules/easey/API.md \
     > _posts/$TAG/0200-01-02-easey.md
 
-cat site/markers.yaml node_modules/markers/API.md \
+cat site/frontmatter/markers.yaml node_modules/markers/API.md \
     > _posts/$TAG/0200-01-03-markers.md
