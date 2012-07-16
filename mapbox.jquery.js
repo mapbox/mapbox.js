093f66ccd30406f6b70e6bad9b761a098adcb130
Tom MacWright <tom@macwright.org>
2012-07-16 14:24:51 -0400

    Remove mapbox.jquery.js

--- mapbox.jquery.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    $.fn.mapbox = function() {
-        return this.map(function(x) {
-            var $this = $(this);
-            if (!$this.data('map')) $(this).data('map', mapbox.map(this));
-            return $this.data('map');
-        });
-    };
-})(jQuery);
