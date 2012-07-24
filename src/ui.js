7e7f6d546505896c7d017d1e619b4beeeea62184
Tom MacWright <tom@macwright.org>
2012-07-24 12:58:13 -0400

    Re-add ui.refresh() with de-duplicated legends and attribution

--- src/ui.js
+++ src/ui.js
@@ -1,7 +1,7 @@
 if (typeof mapbox === 'undefined') mapbox = {};
 
 mapbox.ui = function(map) {
-    return {
+    var ui = {
         zoomer: wax.mm.zoomer().map(map).smooth(true),
         pointselector: wax.mm.pointselector().map(map),
         hash: wax.mm.hash().map(map),
@@ -10,4 +10,33 @@ mapbox.ui = function(map) {
         legend: wax.mm.legend().map(map),
         attribution: wax.mm.attribution().map(map)
     };
+
+    function unique(x) {
+        var u = {}, l = [];
+        for (var i = 0; i < x.length; i++) u[x[i]] = true;
+        for (var a in u) { if (a) l.push(a); }
+        return l;
+    }
+
+    ui.refresh = function() {
+        if (!map) return console && console.error('ui not attached to map');
+
+        var attributions = [], legends = [];
+        for (var i = 0; i < map.layers.length; i++) {
+            if (map.layers[i].tilejson) {
+                var attribution = map.layers[i].tilejson().attribution;
+                if (attribution) attributions.push(attribution);
+                var legend = map.layers[i].tilejson().legend;
+                if (legend) legends.push(legend);
+            }
+        }
+
+        var unique_attributions = unique(attributions);
+        var unique_legends = unique(legends);
+
+        ui.attribution.content(unique_attributions.length ? unique_attributions.join('<br />') : '');
+        ui.legend.content(unique_legends.length ? unique_legends.join('<br />') : '');
+    };
+
+    return ui;
 };
