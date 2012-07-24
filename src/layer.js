0bc37de365311c9c5b8f6718477fca0cc8ce9d83
Ansis Brammanis <ansis.brammanis@gmail.com>
2012-07-24 13:17:54 -0400

    Default to id from tilejson

--- src/layer.js
+++ src/layer.js
@@ -58,6 +58,9 @@ mapbox.layer.prototype.tilejson = function(x) {
     this.setProvider(new wax.mm._provider(x));
     this._tilejson = x;
 
+    this.name = this.name || x.id;
+    this._id = this._id || x.id;
+
     if (x.bounds) {
         var proj = new MM.MercatorProjection(0,
             MM.deriveTransformation(
