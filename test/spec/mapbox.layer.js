959f8f23e8aeadf7a2846f4490c0a26e2982cd93
Tom MacWright <tom@macwright.org>
2012-07-25 11:08:42 -0400

    Make layer gif update

--- test/spec/mapbox.layer.js
+++ test/spec/mapbox.layer.js
@@ -45,7 +45,7 @@ describe("mapbox.layer", function() {
   it("mapbox.layer can create layers", function() {
       expect(mapbox.layer().tilejson()).toEqual({});
       expect(mapbox.layer().provider.options.tiles[0])
-        .toEqual('data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=');
+        .toEqual('data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
   });
 
   it("mapbox.layer has named and name", function() {
