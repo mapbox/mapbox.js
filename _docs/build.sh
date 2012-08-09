ec596b9c53bb48d4ee61e2a14ee96eb4bba829e4
Ansis Brammanis <ansis.brammanis@gmail.com>
2012-08-09 16:57:02 -0400

    Clean up build script

--- _docs/build.sh
+++ _docs/build.sh
@@ -2,33 +2,37 @@
 TAG=$1
 
 if [ -z $TAG ]; then
-    echo "Usage: build-site.sh <tag>"
+    echo "Usage: build.sh <tag>"
+    echo "Tag must be in the form of v0.6.4"
     exit;
 fi
 
-if [ -e _posts/$TAG ]; then
-    echo "rm -r ../_posts/$TAG if you want to re-build"
+if ! [[ $TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Tag must be in the form of v0.6.4"
     exit;
+fi
+
+if [ -e ../_posts/api/0200-01-01-$TAG.html ]; then
+    echo "rm -r ../_posts/$TAG if you want to re-build"
+    #exit;
 fi
 
 echo "--- BUILDING mapbox.js $TAG ---"
-
-echo "Creating build/"
-mkdir -p build
 
 
 echo "Installing..."
 npm install mapbox.js@$TAG
 
-
-echo "Generating html..."
-node generate.js node_modules/mapbox.js/API.md build/mapboxjs.html build/nav-mapboxjs
-node generate.js node_modules/mapbox.js/node_modules/markers/API.md build/markersjs.html build/nav-markersjs
-node generate.js node_modules/mapbox.js/node_modules/easey/API.md build/easey.html build/nav-easey
-
 echo "Creating _posts/api/...$TAG"
 sed "s/__TAG__/$TAG/" header > ../_posts/api/0200-01-01-$TAG.html
-cat build/nav-mapboxjs build/nav-markersjs build/nav-easey >> ../_posts/api/0200-01-01-$TAG.html
-echo -e "version: $TAG\n---\n" >> ../_posts/api/0200-01-01-$TAG.html
-cat build/mapboxjs.html build/markersjs.html build/easey.html >> ../_posts/api/0200-01-01-$TAG.html
+echo -e "version: $TAG" >> ../_posts/api/0200-01-01-$TAG.html
 
+echo "Generating html..."
+if ! `node generate.js \
+    node_modules/mapbox.js/API.md \
+    node_modules/mapbox.js/node_modules/markers/API.md \
+    node_modules/mapbox.js/node_modules/easey/API.md \
+    >> ../_posts/api/0200-01-01-$TAG.html`; then 
+
+    rm ../_posts/api/0200-01-01-$TAG.html;
+fi
--- _docs/generate.js
+++ _docs/generate.js
@@ -1,90 +1,100 @@
-var marked = require('marked'),
-    fs = require('fs');
+var fs = require('fs');
 
-var f = fs.readFileSync(process.argv[2], 'utf8'),
-    lexed = marked.lexer(f);
+try {
+    var marked = require('marked');
+} catch(e) {
+    console.error("Missing dependency: marked");
+    console.error("Install using `npm install`");
+    process.exit(1);
+}
 
-var start = 0,
-    l,
-    anchor,
-    matched,
-    toParse,
+
+
+var start, l, anchor, matched, toParse,
     out = '',
     nav = '';
 
+for (var j = 2; j < process.argv.length; j ++) {
 
-out += '<div>';
+var f = fs.readFileSync(process.argv[j], 'utf8'),
+    lexed = marked.lexer(f);
 
-for (var i = 0; i < lexed.length; i++) {
+    out += '<div>';
+    start = 0;
 
-    l = lexed[i];;
+    for (var i = 0; i < lexed.length; i++) {
 
-    matchedHeading = l.type === 'heading' && l.text.match(/(.*)\.([^\(]*)(\((.*)\))?/);
-    matchedEvent = l.type === 'heading' && l.text.match(/Event:\s(.*)/);
-    matchedSep = l.type === 'html' && l.text.match(/class=.separator.*>(.*)</);
+        l = lexed[i];;
 
-    if (l.type === 'heading' || matchedSep) {
+        matchedHeading = l.type === 'heading' && l.text.match(/(.*)\.([^\(]*)(\((.*)\))?/);
+        matchedEvent = l.type === 'heading' && l.text.match(/Event:\s(.*)/);
+        matchedSep = l.type === 'html' && l.text.match(/class=.separator.*>(.*)</);
 
-        toParse = lexed.slice(start, i);
-        toParse.links = lexed.links;
-        out += marked.parser(toParse);
+        if (l.type === 'heading' || matchedSep) {
 
-        // End previous group
-        out += '</div>';
+            toParse = lexed.slice(start, i);
+            toParse.links = lexed.links;
+            out += marked.parser(toParse);
 
-        // Header is a function or property
-        if (matchedHeading) {
-            anchor = matchedHeading[1] + '.' + matchedHeading[2];
-            out += '<h' + l.depth + ' id="' + anchor + '">';
-            out += anchor;
+            // End previous group
+            out += '</div>';
 
-            if (matchedHeading[3]) {
-                out += '<span class="bracket">(</span>';
-                out += '<span class="args">' + matchedHeading[4] + '</span>';
-                out += '<span class="bracket">)</span>';
+            // Header is a function or property
+            if (matchedHeading) {
+                anchor = matchedHeading[1] + '.' + matchedHeading[2];
+                out += '<h' + l.depth + ' id="' + anchor + '">';
+                out += anchor;
+
+                if (matchedHeading[3]) {
+                    out += '<span class="bracket">(</span>';
+                    out += '<span class="args">' + matchedHeading[4] + '</span>';
+                    out += '<span class="bracket">)</span>';
+                }
+
+                out += '</h' + l.depth + '>\n';
+
+                // Add to navigation tree
+                if (l.depth == 2) {
+                    nav += '- ' + anchor + ':\n';
+                } else if (l.depth == 3) {
+                    nav += '  - ' + anchor + '\n';
+                }
+
+            // Header is for an event
+            } else if (matchedEvent) {
+                anchor = 'Event_"' + matchedEvent[1] + '"';
+                out += '<h' + l.depth + " id='" + anchor + "'>";
+                out += l.text;
+                out += '</h' + l.depth + '>\n';
+                nav += '  - Event "' + matchedEvent[1] + '"\n';
+
+            // Separator
+            } else if (matchedSep) {
+                anchor = matchedSep[1].replace(' ', '_');
+                out += '<div class="separator" id="' + anchor + '">' + matchedSep[1] + '</div>';
+                nav += '  - Separator: ' + matchedSep[1] + '\n';
+                l.depth = 0;
+
+            } else {
+                out += '<h' + l.depth + '>';
+                out += l.text;
+                out += '</h' + l.depth + '>\n';
             }
 
-            out += '</h' + l.depth + '>\n';
+            start = i + 1;
 
-            // Add to navigation tree
-            if (l.depth == 2) {
-                nav += '- ' + anchor + ':\n';
-            } else if (l.depth == 3) {
-                nav += '  - ' + anchor + '\n';
-            }
-
-        // Header is for an event
-        } else if (matchedEvent) {
-            anchor = 'Event_"' + matchedEvent[1] + '"';
-            out += '<h' + l.depth + " id='" + anchor + "'>";
-            out += l.text;
-            out += '</h' + l.depth + '>\n';
-            nav += '  - Event "' + matchedEvent[1] + '"\n';
-
-        // Separator
-        } else if (matchedSep) {
-            anchor = matchedSep[1].replace(' ', '_');
-            out += '<div class="separator" id="' + anchor + '">' + matchedSep[1] + '</div>';
-            nav += '  - Separator: ' + matchedSep[1] + '\n';
-            l.depth = 0;
-
-        } else {
-            out += '<h' + l.depth + '>';
-            out += l.text;
-            out += '</h' + l.depth + '>\n';
+            // End header and start next group
+            out += '<div id="content-' + anchor + '"class="depth-' + l.depth + '">';
         }
-
-        start = i + 1;
-
-        // End header and start next group
-        out += '<div id="content-' + anchor + '"class="depth-' + l.depth + '">';
     }
+
+    toParse = lexed.slice(start, i);
+    toParse.links = lexed.links;
+    out += marked.parser(toParse);
+    out += '</div>';
 }
 
-toParse = lexed.slice(start, i);
-toParse.links = lexed.links;
-out += marked.parser(toParse);
-out += '</div>';
-
-fs.writeFileSync(process.argv[3], out);
-fs.writeFileSync(process.argv[4], nav);
+console.log("navigation:");
+console.log(nav);
+console.log("---");
+console.log(out);
