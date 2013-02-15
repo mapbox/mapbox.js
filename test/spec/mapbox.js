863a9a10d14f0f8e1ad0890e5d063f6ffe5fb465
John Firebaugh <john.firebaugh@gmail.com>
2013-02-15 09:11:15 -0800

    Test framework

---
+++ test/spec/mapbox.js
@@ -0,0 +1,10 @@
+describe("mapbox.base", function() {
+    it("returns 'http://a.tiles.mapbox.com/v3/'", function() {
+        expect(mapbox.base()).to.equal('http://a.tiles.mapbox.com/v3/');
+    });
+
+    it("returns a subdomain based on the number", function() {
+        expect(mapbox.base(1)).to.equal('http://b.tiles.mapbox.com/v3/');
+        expect(mapbox.base(6)).to.equal('http://c.tiles.mapbox.com/v3/');
+    });
+});
