16da246fa674e738bf37a37a94a9dc7c4c6f17a3
Tom MacWright <tom@macwright.org>
2013-02-15 14:56:06 -0500

    Add blanktile ex

--- src/layer.js
+++ src/layer.js
@@ -2,6 +2,10 @@
 //
 // related to [vladimir's example](https://gist.github.com/mourner/1804938)
 // and [leaflet-providers](https://github.com/seelmann/leaflet-providers)
-L.TileLayer.MapBox = L.TileLayer.Common.extend({
-    url: 'http://{s}.tiles.mapbox.com/v3/{user}.{map}/{z}/{x}/{y}.png'
+L.TileLayer.MapBox = L.TileLayer.extend({
+    initialize: function (options) {
+		L.TileLayer.prototype.initialize.call(this,
+            'http://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png',
+            options);
+	}
 });
