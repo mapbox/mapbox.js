613ea4e9f9d51f7166073f05422652fca89a5d5b
John Firebaugh <john.firebaugh@gmail.com>
2013-02-15 14:19:20 -0800

    Add TileJSON data handling

--- src/tilejson.js
+++ src/tilejson.js
@@ -1,18 +1,26 @@
-L.TileJSON = {};
-
-L.TileJSON.Layer = L.LayerGroup.extend({
-    initialize: function(url) {
-        L.LayerGroup.prototype.initialize.call(this);
-        var that = this;
+L.TileJSON = {
+    load: function(url, callback) {
         reqwest({
             url: url,
             type: 'json',
             crossOrigin: true,
-            success: function(json) { that._initialize(json); }
-        })
+            success: callback,
+            error: callback
+        });
+    }
+};
+
+L.TileJSON.Layer = L.LayerGroup.extend({
+    initialize: function(url) {
+        var that = this;
+
+        L.LayerGroup.prototype.initialize.call(this);
+        L.TileJSON.load(url, function(json) { that._initialize(json); });
     },
 
     _initialize: function(json) {
+        var that = this;
+
         json.zoom = json.center[2];
         json.center = L.latLng(json.center[1], json.center[0]);
 
@@ -24,6 +32,20 @@ L.TileJSON.Layer = L.LayerGroup.extend({
             attribution: json.attribution,
             maxZoom: json.maxzoom
         }));
+
+        if (json.data) {
+            for (var i = 0; i < json.data.length; i++) {
+                var url = json.data[i].replace(/\.(geo)?jsonp(?=$|\?)/, '.$1json');
+                L.TileJSON.load(url, function(data) {
+                    that.addLayer(L.geoJson(data, {
+                        pointToLayer: mapbox.markers.style,
+                        onEachFeature: function(feature, layer) {
+                            layer.bindPopup(feature.properties.title);
+                        }
+                    }));
+                });
+            }
+        }
     }
 });
 
