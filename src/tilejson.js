a006857f0085b3f7a5b283febac535787cc8c13c
John Firebaugh <john.firebaugh@gmail.com>
2013-02-15 15:52:56 -0800

    Testing, add getTileUrl hack

--- src/tilejson.js
+++ src/tilejson.js
@@ -11,27 +11,49 @@ L.TileJSON = {
 };
 
 L.TileJSON.Layer = L.LayerGroup.extend({
-    initialize: function(url) {
-        var that = this;
-
+    initialize: function(urlOrJson) {
         L.LayerGroup.prototype.initialize.call(this);
-        L.TileJSON.load(url, function(json) { that._initialize(json); });
+
+        if (urlOrJson && urlOrJson.tilejson) {
+            this._initialize(urlOrJson);
+        } else if (urlOrJson) {
+            var that = this;
+            L.TileJSON.load(urlOrJson, function(json) {
+                that._initialize(json);
+            });
+        }
     },
 
     _initialize: function(json) {
-        var that = this;
-
-        json.zoom = json.center[2];
-        json.center = L.latLng(json.center[1], json.center[0]);
+        var that = this,
+            zoom = json.center[2],
+            center = L.latLng(json.center[1], json.center[0]);
 
         if (this._map) {
-            this._map.setView(json.center, json.zoom);
+            this._map.setView(center, zoom);
         }
 
-        this.addLayer(L.tileLayer(json.tiles[0], {
+        var tileLayer = new L.TileLayer(undefined, {
             attribution: json.attribution,
-            maxZoom: json.maxzoom
-        }));
+            minZoom: json.minzoom,
+            maxZoom: json.maxzoom,
+            tms: json.scheme === 'tms'
+        });
+
+        tileLayer.getTileUrl = function (tilePoint) {
+            this._adjustTilePoint(tilePoint);
+
+            var index = (tilePoint.x + tilePoint.y) % json.tiles.length,
+                url = json.tiles[index];
+
+            return L.Util.template(url, L.extend({
+                z: this._getZoomForUrl(),
+                x: tilePoint.x,
+                y: tilePoint.y
+            }, this.options));
+        };
+
+        this.addLayer(tileLayer);
 
         if (json.data) {
             for (var i = 0; i < json.data.length; i++) {
---
+++ test/spec/tilejson.js
@@ -0,0 +1,72 @@
+describe("L.TileJSON", function() {
+    describe("#load", function() {
+        it("loads TileJSON from the given URL using XHR+CORS", function(done) {
+            L.TileJSON.load('data/tilejson.json', function(data) {
+                expect(data.id).to.equal('examples.map-zr0njcqy');
+                done();
+            });
+        });
+
+        it("loads TileJSON from the given URL using XHR+JSONP");
+    });
+
+    describe("Layer", function() {
+        var tileJSON = {
+            'tilejson': '2.0.0',
+            'attribution': 'Terms & Feedback',
+            'center': [-77.046, 38.907, 12],
+            'minzoom': 1,
+            'maxzoom': 11,
+            'tiles': [
+                'http://a.tiles.mapbox.com/v3/examples.map-zr0njcqy/{z}/{x}/{y}.png',
+                'http://b.tiles.mapbox.com/v3/examples.map-zr0njcqy/{z}/{x}/{y}.png',
+                'http://c.tiles.mapbox.com/v3/examples.map-zr0njcqy/{z}/{x}/{y}.png',
+                'http://d.tiles.mapbox.com/v3/examples.map-zr0njcqy/{z}/{x}/{y}.png'
+            ]
+        };
+
+        function layersOf(layerGroup) {
+            var result = [];
+            layerGroup.eachLayer(function(layer) {
+                result.push(layer);
+            });
+            return result;
+        }
+
+        it("creates a TileLayer with the appropriate min and max zoom", function() {
+            var group = new L.TileJSON.Layer(tileJSON),
+                layers = layersOf(group);
+
+            expect(layers[0].options.minZoom).to.equal(1);
+            expect(layers[0].options.maxZoom).to.equal(11);
+        });
+
+        it("creates a TileLayer with the appropriate attribution", function() {
+            var group = new L.TileJSON.Layer(tileJSON),
+                layers = layersOf(group);
+
+            expect(layers[0].options.attribution).to.equal('Terms & Feedback');
+        });
+
+        it("creates a TileLayer with the appropriate tms option", function() {
+            var group = new L.TileJSON.Layer(L.extend({}, tileJSON, {scheme: 'tms'})),
+                layers = layersOf(group);
+
+            expect(layers[0].options.tms).to.equal(true);
+        });
+
+        it("customizes the TileLayer's getTileUrl method", function() {
+            var map = L.map(document.createElement('div')).setView([0, 0], 3),
+                group = new L.TileJSON.Layer(tileJSON),
+                layer = layersOf(group)[0];
+
+            map.addLayer(layer);
+
+            expect(layer.getTileUrl({x: 0, y: 0})).to.equal('http://a.tiles.mapbox.com/v3/examples.map-zr0njcqy/3/0/0.png');
+            expect(layer.getTileUrl({x: 1, y: 0})).to.equal('http://b.tiles.mapbox.com/v3/examples.map-zr0njcqy/3/1/0.png');
+            expect(layer.getTileUrl({x: 2, y: 0})).to.equal('http://c.tiles.mapbox.com/v3/examples.map-zr0njcqy/3/2/0.png');
+            expect(layer.getTileUrl({x: 3, y: 0})).to.equal('http://d.tiles.mapbox.com/v3/examples.map-zr0njcqy/3/3/0.png');
+            expect(layer.getTileUrl({x: 4, y: 0})).to.equal('http://a.tiles.mapbox.com/v3/examples.map-zr0njcqy/3/4/0.png');
+        });
+    });
+});
