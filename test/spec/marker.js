d77b7ef875773dba386739d7135e59682c9859ba
Tom MacWright <tom@macwright.org>
2013-02-18 13:41:06 -0500

    Test and include marker code

---
+++ test/spec/marker.js
@@ -0,0 +1,52 @@
+describe("mapbox.marker", function() {
+	describe('mapbox.marker.style', function() {
+		it("produces a small marker", function() {
+			var marker = mapbox.marker.style({
+				properties: {
+					'marker-size': 'small'
+				}
+			});
+			expect(marker.options.icon.options.iconUrl).to.contain('pin-s');
+		});
+		it("produces a medium marker", function() {
+				var marker = mapbox.marker.style({
+				properties: {
+					'marker-size': 'medium'
+				}
+			});
+			expect(marker.options.icon.options.iconUrl).to.contain('pin-m');
+		});
+		it("produces a red marker", function() {
+				var marker = mapbox.marker.style({
+				properties: {
+					'marker-color': 'f00'
+				}
+			});
+			expect(marker.options.icon.options.iconUrl).to.contain('f00');
+		});
+
+        it('integrates with leaflet', function() {
+            var geoJson = {
+                type: 'FeatureCollection',
+                features: [{
+                    type: 'Feature',
+                    properties: {
+                        title: 'foo',
+                        'marker-color': '#f00',
+                        'marker-size': 'large'
+                    },
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [-77.0203, 38.8995]
+                    }
+                }]
+            };
+
+            expect(function() {
+                L.geoJson(geoJson, {
+                    pointToLayer: mapbox.marker.style
+                });
+            }).to.not.throwException();
+        });
+	});
+});
