c13495077200306f26f641c7d1ce1aa7ae798e55
Tom MacWright <tom@macwright.org>
2013-02-18 12:13:31 -0500

    Sketch out support for HTTPS

--- src/mapbox.js
+++ src/mapbox.js
@@ -1,11 +1,30 @@
 var mapbox = {};
 
+mapbox.HTTP_URLS = [
+    'http://a.tiles.mapbox.com/v3/',
+    'http://b.tiles.mapbox.com/v3/',
+    'http://c.tiles.mapbox.com/v3/',
+    'http://d.tiles.mapbox.com/v3/'];
+
+mapbox.HTTPS_URLS = [];
+
+// Return the base url of a specific version of MapBox's API.
+//
+// `hash`, if provided must be a number and is used to distribute requests
+// against multiple `CNAME`s in order to avoid connection limits in browsers
 mapbox.base = function(hash) {
-    var subdomains = ['a', 'b', 'c', 'd'];
+    // By default, use public HTTP urls
+    var urls = mapbox.HTTP_URLS;
+
+    // Support HTTPS if the user has specified HTTPS urls to use, and this
+    // page is under HTTPS
+    if (window.location.protocol === 'https:' && mapbox.HTTPS_URLS.length) {
+        urls = mapbox.HTTPS_URLS;
+    }
+
     if (hash === undefined || typeof hash !== 'number') {
-        return 'http://a.tiles.mapbox.com/v3/';
+        return urls[0];
     } else {
-        return 'http://' +
-            subdomains[hash % subdomains.length] + '.tiles.mapbox.com/v3/';
+        return urls[hash % urls.length];
     }
 };
