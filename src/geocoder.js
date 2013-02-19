6158ca7d384f2800a0e3006d6dffa4327d34fa26
John Firebaugh <john.firebaugh@gmail.com>
2013-02-18 18:49:40 -0800

    Add L.Control.MapBoxGeocoder (#179)

---
+++ src/geocoder.js
@@ -0,0 +1,54 @@
+L.Control.MapBoxGeocoder = L.Control.extend({
+    initialize: function (_) {
+        // map id 'tmcw.foo'
+        if (_.indexOf('/') == -1) this.id(_);
+        // url 'http://foo.com/foo.bar'
+        else this.url(_);
+    },
+
+    url: function(url) {
+        this.url = url;
+        return this;
+    },
+
+    id: function(id) {
+        return this.url(mapbox.base() + id + '/geocode/{query}.json');
+    },
+
+    onAdd: function (map) {
+        this._map = map;
+
+        var className = 'leaflet-control-mapbox-geocoder',
+            container = L.DomUtil.create('div', className);
+
+        L.DomEvent.disableClickPropagation(container);
+
+        var form = this._form = L.DomUtil.create('form', className + '-form');
+        L.DomEvent.addListener(form, 'submit', this._geocode, this);
+
+        var input = this._input = document.createElement('input');
+        input.type = 'text';
+
+        var submit = this._submit = document.createElement('input');
+        submit.type = 'submit';
+        submit.value = 'Locate';
+
+        form.appendChild(input);
+        form.appendChild(submit);
+        container.appendChild(form);
+
+        return container;
+    },
+
+    _geocode: function (event) {
+        L.DomEvent.preventDefault(event);
+
+        var query = encodeURIComponent(this._input.value);
+
+        L.TileJSON.load(L.Util.template(this.url, {query: query}), L.bind(function(err, json) {
+            if (json && json.results && json.results.length) {
+                this._map.setView([json.results[0][0].lat, json.results[0][0].lon], 6);
+            }
+        }, this));
+    }
+});
---
+++ test/spec/geocoder.js
@@ -0,0 +1,43 @@
+describe('L.Control.MapBoxGeocoder', function() {
+    var server;
+
+    beforeEach(function() {
+        server = sinon.fakeServer.create();
+    });
+
+    afterEach(function() {
+        server.restore();
+    });
+
+    var json = {
+        "query": ["austin"],
+        "results": [[{
+            "bounds": [-97.9383829999999, 30.098659, -97.5614889999999, 30.516863],
+            "lat": 30.3071816,
+            "lon": -97.7559964,
+            "name": "Austin",
+            "score": 600000790107194.8,
+            "type": "city",
+            "id": "mapbox-places.4201"
+        }]]};
+
+    it('performs forward geolocation, centering the map on the first result', function() {
+        var map = new L.Map(document.createElement('div')),
+            control = new L.Control.MapBoxGeocoder('http://api.tiles.mapbox.com/v3/examples.map-vyofok3q/geocode/{query}.json').addTo(map);
+
+        server.respondWith('GET',
+            'http://api.tiles.mapbox.com/v3/examples.map-vyofok3q/geocode/austin.json',
+            [200, { "Content-Type": "application/json" }, JSON.stringify(json)]);
+
+        control._input.value = 'austin';
+        happen.click(control._submit);
+        server.respond();
+
+        expect(map.getCenter()).to.be.near({lat: 30.3, lng: -97.7}, 1e-1);
+    });
+
+    xit('sets url based on an id', function() {
+        var control = new L.Control.MapBoxGeocoder('examples.map-vyofok3q');
+        expect(control.url).to.equal('http://a.tiles.mapbox.com/v3/examples.map-vyofok3q/geocode/{query}.json');
+    });
+});
