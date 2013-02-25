baf51953da2edc7039bbc6111a7fe49ebc3ab704
Tom MacWright <tom@macwright.org>
2013-02-25 17:31:12 -0500

    Add interaction control with formatting and display, and configurability

--- src/interaction_control.js
+++ src/interaction_control.js
@@ -1,7 +1,39 @@
 mapbox.interactionControl = L.Control.extend({
 
-    initialize: function(_) {
+    options: {
+        mapping: {
+            mousemove: ['teaser'],
+            click: ['full'],
+            mouseout: [function() { return ''; }]
+        }
     },
+
+    initialize: function(_, options) {
+        L.Util.setOptions(this, options);
+        this._layer = _;
+    },
+
+    _handler: function(type, o) {
+        var mapping = this.options.mapping[type];
+        for (var i = 0; i < mapping.length; i++) {
+            if (typeof mapping[i] === 'function') {
+                var res = mapping[i](o);
+                if (typeof res === 'string') {
+                    this._container.innerHTML = res;
+                }
+            } else if (o[mapping[i]]) {
+                if (mapping[i] === 'location') {
+                    window.top.location.href = o[mapping[i]];
+                } else {
+                    this._container.innerHTML = o[mapping[i]];
+                }
+            }
+        }
+    },
+
+    _mousemove: function(o) { this._handler('mousemove', o); },
+    _mouseout: function(o) { this._handler('mouseout', o); },
+    _click: function(o) { this._handler('click', o); },
 
     onAdd: function(map) {
         this._map = map;
@@ -10,6 +42,11 @@ mapbox.interactionControl = L.Control.extend({
             container = L.DomUtil.create('div', className);
 
         L.DomEvent.disableClickPropagation(container);
+
+        this._layer
+            .on('mousemove', this._mousemove, this)
+            .on('mouseout', this._mouseout, this)
+            .on('mousemove', this._click, this);
 
         return container;
     }
