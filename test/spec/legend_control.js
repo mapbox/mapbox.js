a2ae27ac55be9c040cc585e1ecd08b3c0c3522de
Tom MacWright <tom@macwright.org>
2013-04-04 11:09:35 -0400

    Improve legend tests

--- test/spec/legend_control.js
+++ test/spec/legend_control.js
@@ -1,24 +1,65 @@
 describe('mapbox.legendControl', function() {
-    it('mapbox.legend constructor', function() {
+    it('constructor', function() {
         var legend = mapbox.legendControl();
         expect(legend).to.be.ok();
     });
 
-    it('addLegend', function() {
-        var legend = mapbox.legendControl();
-        expect(legend.addLegend('foo')).to.eql(legend);
+    describe('#addLegend', function() {
+
+        it('returns the legend object', function() {
+            var legend = mapbox.legendControl();
+            expect(legend.addLegend('foo')).to.eql(legend);
+        });
+
+        it('adds a map legend element to its container', function() {
+            var elem = document.createElement('div');
+            var map = mapbox.map(elem);
+            var legend = mapbox.legendControl();
+            legend.addTo(map);
+            expect(legend.addLegend('foo')).to.eql(legend);
+            expect(legend._container.innerHTML).to.eql('<div class="map-legend">foo</div>');
+        });
+
+        it('handles multiple legends', function() {
+            var elem = document.createElement('div');
+            var map = mapbox.map(elem);
+            var legend = mapbox.legendControl();
+            expect(legend.addTo(map)).to.eql(legend);
+            expect(legend.addLegend('foo')).to.eql(legend);
+            expect(legend.addLegend('bar')).to.eql(legend);
+            expect(legend._container.innerHTML).to.eql('<div class="map-legend">foo</div><div class="map-legend">bar</div>');
+        });
+
     });
 
-    it('removeLegend', function() {
-        var legend = mapbox.legendControl();
-        expect(legend.addLegend('foo')).to.eql(legend);
-        expect(legend.removeLegend('foo')).to.eql(legend);
-        expect(legend.removeLegend()).to.eql(legend);
+    describe('#removeLegend', function() {
+        it('returns the legend object', function() {
+            var legend = mapbox.legendControl();
+            expect(legend.addLegend('foo')).to.eql(legend);
+            expect(legend.removeLegend('foo')).to.eql(legend);
+            expect(legend.removeLegend()).to.eql(legend);
+        });
+
+        it('adds and removes dom elements', function() {
+            var elem = document.createElement('div');
+            var map = mapbox.map(elem);
+            var legend = mapbox.legendControl();
+            legend.addTo(map);
+            expect(legend.addLegend('foo')).to.eql(legend);
+            expect(legend.addLegend('bar')).to.eql(legend);
+            expect(legend._container.innerHTML).to.eql('<div class="map-legend">foo</div><div class="map-legend">bar</div>');
+            expect(legend.removeLegend('bar')).to.eql(legend);
+            expect(legend._container.innerHTML).to.eql('<div class="map-legend">foo</div>');
+        });
     });
 
     it('sanitizes its content', function() {
-        var map = L.Map(document.createElement('div'));
-        var control = new mapbox.legendControl();
+        var elem = document.createElement('div');
+        var map = L.map(elem);
+        var legend = new mapbox.legendControl();
+        legend.addTo(map);
+        expect(legend.addLegend('<script></script>')).to.eql(legend);
+        expect(legend._container.innerHTML).to.eql('<div class="map-legend"></div>');
     });
 
     it('turns sanitization off', function() {
