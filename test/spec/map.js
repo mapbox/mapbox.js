4813c291d61bcf95f97f2042c21818fc1eaabaa6
Tom MacWright <tom@macwright.org>
2013-04-04 10:57:21 -0400

    Test for inheriting re-init check, fixes #260

--- test/spec/map.js
+++ test/spec/map.js
@@ -1,4 +1,4 @@
-describe("mapbox.map", function() {
+describe('mapbox.map', function() {
     var server, element, tileJSON = helpers.tileJSON;
 
     beforeEach(function() {
@@ -10,33 +10,52 @@ describe("mapbox.map", function() {
         server.restore();
     });
 
-    it("allows access to the tilejson object after assignment", function() {
+    it('allows access to the tilejson object after assignment', function() {
         var layer = new mapbox.map(element, tileJSON);
         expect(layer.getTileJSON()).to.equal(tileJSON);
     });
 
-    it("adds a tile layer immediately", function() {
-        var group = new mapbox.map(element, 'data/tilejson.json');
-        expect(group.tileLayer).to.be.ok();
+    describe('layers', function() {
+
+        it('adds a tile layer immediately', function() {
+            var group = new mapbox.map(element, 'data/tilejson.json');
+            expect(group.tileLayer).to.be.ok();
+        });
+
+        it('initializes the tile layer', function() {
+            var group = new mapbox.map(element, tileJSON);
+            expect(group.tileLayer.getTileJSON()).to.equal(tileJSON);
+        });
+
+        it('adds a data layer immediately', function() {
+            var group = new mapbox.map(element, 'data/tilejson.json');
+            expect(group.markerLayer).to.be.ok();
+        });
+
+        it('adds a grid layer immediately', function() {
+            var group = new mapbox.map(element, 'data/tilejson.json');
+            expect(group.gridLayer).to.be.ok();
+        });
+
+        it('initializes the grid layer', function() {
+            var group = new mapbox.map(element, tileJSON);
+            expect(group.gridLayer.getTileJSON()).to.equal(tileJSON);
+        });
+
     });
 
-    it("initializes the tile layer", function() {
-        var group = new mapbox.map(element, tileJSON);
-        expect(group.tileLayer.getTileJSON()).to.equal(tileJSON);
-    });
+    describe('corner cases', function() {
 
-    it("adds a data layer immediately", function() {
-        var group = new mapbox.map(element, 'data/tilejson.json');
-        expect(group.markerLayer).to.be.ok();
-    });
+        it('re-initialization throws', function() {
+            var group = new mapbox.map(element, tileJSON);
+            expect(group.tileLayer.getTileJSON()).to.equal(tileJSON);
 
-    it("adds a grid layer immediately", function() {
-        var group = new mapbox.map(element, 'data/tilejson.json');
-        expect(group.gridLayer).to.be.ok();
-    });
+            expect(function() {
+                new mapbox.map(element, tileJSON);
+            }).to.throwException(function(e) {
+                expect(e.message).to.eql('Map container is already initialized.');
+            });
+        });
 
-    it("initializes the grid layer", function() {
-        var group = new mapbox.map(element, tileJSON);
-        expect(group.gridLayer.getTileJSON()).to.equal(tileJSON);
     });
 });
