efe8eb7a815dd29d2a632d602e778c23c54ed9ff
John Firebaugh <john.firebaugh@gmail.com>
2013-04-05 13:27:58 -0700

    Remove unused

--- src/start.js
+++
@@ -1,9 +0,0 @@
-(function(root, factory) {
-    if (typeof exports === 'object') {
-        module.exports = factory();
-    } else if (typeof define === 'function' && define.amd) {
-        define(factory);
-    } else {
-        root.mapbox = factory();
-    }
-}(this, function() {
