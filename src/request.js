28eb2f2c94ddb76b55eaa4110204218a654829e1
John Firebaugh <john.firebaugh@gmail.com>
2013-04-05 15:34:06 -0700

    Use util.strict

--- src/request.js
+++ src/request.js
@@ -1,12 +1,8 @@
 var corslite = require('corslite'),
-    JSON3 = require('json3');
+    JSON3 = require('json3'),
+    strict = require('./util').strict;
 
 module.exports = function(url, callback) {
-    function strict(_, type) {
-        if (typeof _ !== type) {
-            throw Error('Invalid argument: ' + type + ' expected');
-        }
-    }
     strict(url, 'string');
     strict(callback, 'function');
     corslite(url, function(err, resp) {
