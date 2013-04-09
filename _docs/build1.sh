78f3810f805bd0c00609d8e7357944fc0e7de24c
Tom MacWright <tom@macwright.org>
2013-04-09 12:51:59 -0400

    Add v1 build script

---
+++ _docs/build1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+TAG=$1
+
+if [ -z $TAG ]; then
+    echo "Usage: build.sh <tag>"
+    echo "Tag must be in the form of v0.6.4"
+    exit;
+fi
+
+if [ -e ../_posts/api/0200-01-01-$TAG.html ]; then
+    echo "rm -r ../_posts/$TAG if you want to re-build"
+    #exit;
+fi
+
+echo "--- BUILDING mapbox.js $TAG ---"
+
+
+echo "Installing..."
+npm install mapbox.js@$TAG
+
+echo "Creating _posts/api/...$TAG"
+sed "s/__TAG__/$TAG/" header > ../_posts/api/0200-01-01-$TAG.html
+echo -e "version: $TAG" >> ../_posts/api/0200-01-01-$TAG.html
+
+echo "Generating html..."
+if ! `node generate.js \
+    node_modules/mapbox.js/API.md \
+    >> ../_posts/api/0200-01-01-$TAG.html`; then 
+
+    rm ../_posts/api/0200-01-01-$TAG.html;
+fi
