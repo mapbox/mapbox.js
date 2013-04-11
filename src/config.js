aca36fcdd71d59d70f365cc8d6a9d35af70c91d5
Tom MacWright <tom@macwright.org>
2013-04-11 15:54:20 -0400

    Add config module

---
+++ src/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    HTTP_URLS: [
+        'http://a.tiles.mapbox.com/v3/',
+        'http://b.tiles.mapbox.com/v3/',
+        'http://c.tiles.mapbox.com/v3/',
+        'http://d.tiles.mapbox.com/v3/'],
+
+    FORCE_HTTPS: false,
+
+    HTTPS_URLS: []
+
+};
