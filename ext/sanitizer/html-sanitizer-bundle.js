54ede791dfee825eeb5466749ae0804ba2433f54
Tom MacWright <tom@macwright.org>
2013-04-11 16:23:58 -0400

    Permit data/image. Fixes #300

--- ext/sanitizer/html-sanitizer-bundle.js
+++ ext/sanitizer/html-sanitizer-bundle.js
@@ -2162,7 +2162,7 @@ var html = (function(html4) {
     });
   }
 
-  var ALLOWED_URI_SCHEMES = /^(?:https?|mailto)$/i;
+  var ALLOWED_URI_SCHEMES = /^(?:https?|mailto|data)$/i;
 
   function safeUri(uri, effect, ltype, hints, naiveUriRewriter) {
     if (!naiveUriRewriter) { return null; }
@@ -2439,14 +2439,11 @@ var html_sanitize = html['sanitize'];
 // html-sanitizer to allow for styling
 html4.ATTRIBS['*::style'] = 0;
 html4.ELEMENTS['style'] = 0;
-
 html4.ATTRIBS['a::target'] = 0;
-
 html4.ELEMENTS['video'] = 0;
 html4.ATTRIBS['video::src'] = 0;
 html4.ATTRIBS['video::poster'] = 0;
 html4.ATTRIBS['video::controls'] = 0;
-
 html4.ELEMENTS['audio'] = 0;
 html4.ATTRIBS['audio::src'] = 0;
 html4.ATTRIBS['video::autoplay'] = 0;
--- src/sanitize.js
+++ src/sanitize.js
@@ -2,7 +2,10 @@ var html_sanitize = require('../ext/sanitizer/html-sanitizer-bundle.js');
 
 // https://bugzilla.mozilla.org/show_bug.cgi?id=255107
 function cleanUrl(url) {
-    if (/^(https?:\/\/|data:image)/.test(url)) return url;
+    if (/^https/.test(url.getScheme())) return url.toString();
+    if ('data' == url.getScheme() && /^image/.test(url.getPath())) {
+        return url.toString();
+    }
 }
 
 function cleanId(id) {
