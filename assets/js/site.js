ac7a0985b8c432392249699a206f054465c9ddd4
tristen <tristen.brown@gmail.com>
2014-02-28 14:19:31 -0500

    Remove stuff hotlinked from mapbox.com

--- assets/js/site.js
+++ assets/js/site.js
@@ -12,39 +12,13 @@ function load() {
         window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
     }
 
-    ZeroClipboard.setDefaults({
-        moviePath: window.location.origin + '/mapbox.js/assets/js/ZeroClipboard.swf',
-        forceHandCursor: true
-    });
-
-    $('.js-clipboard').each(function() {
-        var $clip = $(this);
-        if (!$clip.data('zeroclipboard-bound')) {
-            $clip.attr('data-clipboard-text', $('#' + $clip.data('ref-id')).text().trim());
-            $clip.data('zeroclipboard-bound', true);
-            var clip = new ZeroClipboard(this);
-            clip.on('complete', function() {
-                var $this = $(this);
-                $this.siblings('input').select();
-                var text = $this.text();
-                $this.text('Copied to clipboard! ');
-                setTimeout(function() {
-                    $this.text(text);
-                }, 1000);
-                var type = (location.pathname.split('plugins').length > 1) ? 'plugin' : 'example';
-                analytics.track('Copied ' + type + ' with clipboard');
-            });
-        }
-    });
-
     $('#docs-search input').swiftype({
         autocompleteContainingElement: $('#docs-search'),
         filters: {
           page: {
             type: ['examples', 'mapboxjs-api', 'mapboxjs-plugins']
           }
-        },
-        engineKey: 'ByE7bq3YmMASdFvY7L4h'
+        }
     });
 }
 
