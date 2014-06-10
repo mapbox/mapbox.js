5bfb15a940237480d264d0658efe78ddf1aaca62
John Firebaugh <john.firebaugh@gmail.com>
2014-06-10 11:45:01 -0700

    Hard-force request protocol in certain situations
    When using XDomainRequest, force the same protocol as the document. XDR
    does not support cross-protocol requests.
    
    Otherwise, when the document protocol is https or FORCE_HTTPS is set,
    force https.
    
    Otherwise, when requesting a protocol-relative URL, force http. This
    makes protocol-relative URLs work when the document protocol is file:.

--- src/request.js
+++ src/request.js
@@ -1,11 +1,25 @@
 'use strict';
 
 var corslite = require('corslite'),
-    strict = require('./util').strict;
+    strict = require('./util').strict,
+    config = require('./config'),
+    protocol = /^(https?:)?(?=\/\/(.|api)\.tiles\.mapbox\.com\/)/;
 
 module.exports = function(url, callback) {
     strict(url, 'string');
     strict(callback, 'function');
+
+    url = url.replace(protocol, function(match, protocol) {
+        if (!('withCredentials' in new XMLHttpRequest())) {
+            // XDomainRequest in use; doesn't support cross-protocol requests
+            return document.location.protocol;
+        } else if ('https:' === protocol || 'https:' === document.location.protocol || config.FORCE_HTTPS) {
+            return 'https:';
+        } else {
+            return 'http:';
+        }
+    });
+
     return corslite(url, onload);
     function onload(err, resp) {
         if (!err && resp) {
