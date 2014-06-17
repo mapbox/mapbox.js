dded49df3990aa4551da2ff69a630c17a7943e2b
tristen <tristen.brown@gmail.com>
2014-06-17 15:32:21 -0400

    API updates to zeroclipboard

--- docs/assets/js/site.js
+++ docs/assets/js/site.js
@@ -12,8 +12,8 @@ function load() {
         window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
     }
 
-    ZeroClipboard.setDefaults({
-        moviePath: window.location.origin + '/mapbox.js/assets/js/ZeroClipboard.swf',
+    ZeroClipboard.config({
+        swfPath: window.location.origin + '/mapbox.js/assets/js/ZeroClipboard.swf',
         forceHandCursor: true
     });
 
@@ -23,19 +23,23 @@ function load() {
             $clip.attr('data-clipboard-text', $('#' + $clip.data('ref-id')).text().trim());
             $clip.data('zeroclipboard-bound', true);
             var clip = new ZeroClipboard(this);
-            clip.on('complete', function() {
-                var $this = $(this);
-                $this.siblings('input').select();
-                var text = $this.text();
-                $this.text('Copied to clipboard! ');
+            clip.on('aftercopy', function() {
+                $clip.siblings('input').select();
+                var text = $clip.text();
+                $clip.text('Copied to clipboard! ');
                 setTimeout(function() {
-                    $this.text(text);
+                    $clip.text(text);
                 }, 1000);
                 var type = (location.pathname.split('plugins').length > 1) ? 'plugin' : 'example';
                 analytics.track('Copied ' + type + ' with clipboard');
             });
         }
     });
+
+    $('.js-clipboard').on('click', function() {
+        return false;
+    });
+
     $('#docs-search input').swiftype({
         autocompleteContainingElement: $('#docs-search'),
         filters: {
