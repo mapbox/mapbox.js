7785b48c939f0ef39371033d55125b456e82973b
John Firebaugh <john.firebaugh@gmail.com>
2014-08-19 13:09:07 -0700

    Include access token in Coffeescript example

--- docs/_plugins/insert_token.rb
+++ docs/_plugins/insert_token.rb
@@ -1,6 +1,7 @@
 module InsertToken
     def insert_token(input)
         input.sub("<script>", "<script>\nL.mapbox.accessToken = '<your access token here>';")
+             .sub("<script type='text/coffeescript'>", "<script type='text/coffeescript'>\nL.mapbox.accessToken = '<your access token here>'")
     end
 end
 
