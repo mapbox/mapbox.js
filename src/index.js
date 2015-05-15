c9f4f871de989c90960600e94f3c429cf9703986
Chris Wheeler <cmwhee@gmail.com>
2015-05-15 16:12:02 -0400

    If at first you don't succeed... use unnecessarily complex syntax

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-require('./leaflet');
-require('./mapbox');
-if(module) module.exports = window.L;
++function(){
+  'use strict';
+
+  require('./leaflet');
+  require('./mapbox');
+  if(module) module.exports = window.L;
+}();
