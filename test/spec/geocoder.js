2fd477ec26c7a729f4feeb72ad9d6556ccf9b789
Tom Lee <thomas.j.lee@gmail.com>
2015-10-14 21:37:54 -0400

    fix tests
    fix sinon fakeserver url

--- test/spec/geocoder.js
+++ test/spec/geocoder.js
@@ -20,11 +20,15 @@ describe('L.mapbox.geocoder', function() {
             var g = L.mapbox.geocoder('mapbox.places');
             expect(g.queryURL({query: ['austin', 'houston'], proximity: [10, 15]}))
                 .to.eql('http://a.tiles.mapbox.com/geocoding/v5/mapbox.places/austin;houston.json?access_token=key&proximity=15,10');
-            expect(g.queryURL({query: ['austin', 'houston'], proximity: L.latLng(10, 15)}))
-                .to.eql('http://a.tiles.mapbox.com/geocoding/v5/mapbox.places/austin;houston.json?access_token=key&proximity=15,10');
+        });
+
+        it('rounds proximity params correctly', function() {
+            var g = L.mapbox.geocoder('mapbox.places');
             expect(g.queryURL({query: ['austin', 'houston'], proximity: L.latLng(-10.12345, 15.67890)}))
                 .to.eql('http://a.tiles.mapbox.com/geocoding/v5/mapbox.places/austin;houston.json?access_token=key&proximity=15.679,-10.123');
         });
+
+        it('rounds reverse ')
     });
 
     describe('#query', function() {
@@ -76,7 +80,7 @@ describe('L.mapbox.geocoder', function() {
     });
 
     describe('#reverseQuery', function() {
-        it('performs reverse geolocation', function() {
+        it('performs reverse geolocation', function(done) {
             var g = L.mapbox.geocoder('mapbox.places');
 
             server.respondWith('GET',
@@ -85,20 +89,22 @@ describe('L.mapbox.geocoder', function() {
 
             g.reverseQuery({ lat: 30.3, lng: -97.7 }, function(err, res) {
                 expect(res).to.eql(helpers.geocoderReverse);
+                done();
             });
 
             server.respond();
         });
 
-        it('rounds reverse coordinates correctly', function() {
+        it('rounds reverse coordinates correctly', function(done) {
             var g = L.mapbox.geocoder('mapbox.places');
 
             server.respondWith('GET',
-                'http://a.tiles.mapbox.com/geocoding/v5/mapbox.places/-97.7%2C30.3.json?access_token=key',
-                [200, { "Content-Type": "application/json" }, JSON.stringify(helpers.geocoderReverse)]);
+                /http:\/\/a\.tiles\.mapbox\.com\/geocoding\/v5\/mapbox.places\/[\-\d\.]+(%2C|,)[\-\d\.]+\.json\?access_token=key/,
+                [200, { "Content-Type": "application/json" }, JSON.stringify(helpers.geocoderReverseRounded)]);
 
             g.reverseQuery({ lat: 30.1234567890, lng: -97.0987654321 }, function(err, res) {
                 expect(res).to.eql(helpers.geocoderReverseRounded);
+                done();
             });
 
             server.respond();
