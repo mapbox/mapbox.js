33c6d22643413e3c11f713aad2a559869e839a0d
Josh Erb <josh.erb@mapbox.com>
2020-01-29 12:38:32 -0500

    add template style mapping to config

--- src/config.js
+++ src/config.js
@@ -4,5 +4,14 @@ module.exports = {
     HTTP_URL: 'http://api.mapbox.com',
     HTTPS_URL: 'https://api.mapbox.com',
     FORCE_HTTPS: true,
-    REQUIRE_ACCESS_TOKEN: true
+    REQUIRE_ACCESS_TOKEN: true,
+    TEMPLATE_STYLES: {
+        'mapbox.dark': 'mapbox/dark-v10',
+        'mapbox.light': 'mapbox/light-v10',
+        'mapbox.osm-bright': 'mapbox/bright-v9',
+        'mapbox.outdoors': 'mapbox/outdoors-v11', 
+        'mapbox.satellite': 'mapbox/satellite-v9',
+        'mapbox.streets': 'mapbox/streets-v11', 
+        'mapbox.streets-basic': 'mapbox/basic-v9'
+    }
 };
