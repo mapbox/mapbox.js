598cd92c092e4426555ad2b4c8ad2a2a2155c30a
Josh Erb <josh.erb@mapbox.com>
2020-01-29 10:54:10 -0500

    change base url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-    HTTP_URL: 'http://a.tiles.mapbox.com/v4',
-    HTTPS_URL: 'https://a.tiles.mapbox.com/v4',
+    HTTP_URL: 'http://api.mapbox.com',
+    HTTPS_URL: 'https://api.mapbox.com',
     FORCE_HTTPS: true,
     REQUIRE_ACCESS_TOKEN: true
 };
